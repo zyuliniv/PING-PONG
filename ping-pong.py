d31a1b88e7c0777b8a1e14c0f7718bd23a00cbdd
zyuliniv <82742092+zyuliniv@users.noreply.github.com>
2021-04-25 10:06:50 +0300

    Add files via upload
    Первоначальный файл. ver.1.0.

---
+++ ping-pong.py
@@ -0,0 +1,74 @@
+from pygame import *
+'''Необходимые классы'''
+
+#класс-родитель для спрайтов 
+class GameSprite(sprite.Sprite):
+    #конструктор класса
+       #конструктор класса
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height): # добавить еще два параметра при создании и задавать размер прямоугольгника для картинки самим
+        super().__init__()
+ 
+        # каждый спрайт должен хранить свойство image - изображение
+        self.image = transform.scale(image.load(player_image), (wight, height)) # вместе 55,55 - параметры
+        self.speed = player_speed
+ 
+        # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+#класс-наследник для спрайта-игрока (управляется стрелками)
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+    def update_l(self):
+
+
+#Игровая сцена:
+back = (200, 255, 255) # цвет фона (background)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+#флаги отвечающие за состояние игры
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
+        
+
+
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+
+    display.update()
+    clock.tick(FPS)
+
+
+
+
