c33c24bcdd71dd4d9631fcc7ccb85b6f710f539f
zyuliniv <82742092+zyuliniv@users.noreply.github.com>
2021-05-02 09:55:31 +0300

    Update ping-pong.py
    Дописали функцию  автоматического движения мяча. Добавили код анализа столкновений мяча с ракетками и стенами игрового поля.

--- ping-pong.py
+++ ping-pong.py
@@ -29,7 +29,11 @@ class Player(GameSprite):
         if keys[K_DOWN] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
     def update_l(self):
-
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
 
 #Игровая сцена:
 back = (200, 255, 255) # цвет фона (background)
@@ -43,6 +47,19 @@ game = True
 finish = False
 clock = time.Clock()
 FPS = 60
+
+#создания мяча и ракетки    
+racket1 = Player('racket.png', 30, 200, 4, 50, 150) # при созданни спрайта добавляется еще два параметра
+racket2 = Player('racket.png', 520, 200, 4, 50, 150)
+ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+
+speed_x = 3
+speed_y = 3
 
 while game:
     for e in event.get():
@@ -60,7 +77,21 @@ while game:
             speed_x *= -1
             speed_y *= 1
         
+        # если мяч достигает границ экрана меняем направление его движения
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
 
+        # если мяч улетел дальше ракетки, выводим условие проигрыша для первого игрока
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+            game_over = True
+
+        # если мяч улетел дальше ракетки, выводим условие проигрыша для второго игрока
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
+            game_over = True
 
         racket1.reset()
         racket2.reset()
@@ -68,7 +99,3 @@ while game:
 
     display.update()
     clock.tick(FPS)
-
-
-
-
